15f883795750c5351279b3652e037e3ae8671052
harinda05 <harindaburndown@gmail.com>
2018-06-06 23:57:15 +0530

    Edited includes

--- header.php
+++ header.php
@@ -13,13 +13,13 @@
 
 	<!-- Header -->
 	<div class="header" >
-		<a href="index.php"><img src="img/logo.png" height="100px" width="150px"></a>
+		<a href="index.php"><img src="/fashion/img/logo.png" height="100px" width="150px"></a>
 		<div class="menu">
 			<ul>
 				<li><a href="#Help">Help</a></li>
 				<li><a href="#contact">Contact</a></li>
-				<li><a href="pages/application/register.php">Resgister</a></li>
-				<li><a href="pages/application/login.php">Log In</a></li>
+				<li><a href="/fashion/pages/applicant/register.php">Resgister</a></li>
+				<li><a href="/fashion/pages/applicant/login.php">Log In</a></li>
 			</ul>
 		</div>
 	</div>
--- pages/applicant/login.php
+++ pages/applicant/login.php
@@ -6,19 +6,19 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link rel="stylesheet" type="text/css" href="../css/styles.css">
+	<link rel="stylesheet" type="text/css" href="../../css/styles.css">
 
 </head>
 <body>
 
 	<!-- header -->
-	<?php include 'header.php'; ?>
+	<?php include '../../header.php'; ?>
 
 	<!-- body -->
 	<div class="main_body">
 		<div class="form">
 			<h1>Log In</h1>
-			<hr style="border: 1.5px solid #00204A; margin-bottom: 30px; width: 50%;>
+			<hr style="border: 1.5px solid #00204A; margin-bottom: 30px; width: 50%;">
 
 			<label for="username"><b>Username</b></label>
     		<input type="text" placeholder="Enter Username" name="username" required>
@@ -38,7 +38,7 @@
 
 
 
-	<?php include 'footer.php' ?>
+	<?php include '../../footer.php' ?>
 
 </body>
 </html>

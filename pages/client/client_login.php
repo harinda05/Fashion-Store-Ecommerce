a7ad2824164d2f43fb65581b128b6185f399058e
Dilanka Perera <dilankaperera95@gmail.com>
2018-06-08 06:32:23 +0530

    creating the admin login

---
+++ pages/client/client_login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Prince Fashions</title>
+
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" type="text/css" href="../css/styles.css">
+
+</head>
+<body>
+
+	
+
+</body>
+</html>
---
+++ pages/client/login2.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Prince Fashions</title>
+
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" type="text/css" href="assests/styles.css">
+
+	<style type="text/css">
+		body{
+			background-color: #00204A;
+		}
+	</style>
+</head>
+<body>
+ 	
+ 	<!-- body -->		
+		<div class="admin_login">
+			<div class="admin_login_heading">
+
+				<h1>ADMIN DASHBOARD LOGIN</h1>
+
+			</div>
+
+			<div class="admin_login_form">
+				<form>
+
+				<label for="username"><b>Username</b></label>
+    			<input type="text" placeholder="Enter Username" name="username" required>
+
+    			<label for="password"><b>Password</b></label>
+    			<input type="password" placeholder="Enter Password" name="password" required>
+
+				<button type="button">Log In</button>
+
+				</form>
+			</div>
+
+			
+			
+		</div>
+
+
+
+
+</body>
+</html>

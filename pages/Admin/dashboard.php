9c15958adb7049cde8226d369e74701985d9f735
harinda05 <harindaburndown@gmail.com>
2018-06-12 11:42:07 +0530

    bc

--- pages/Admin/dashboard.php
+++ pages/Admin/dashboard.php
@@ -6,7 +6,7 @@
 		<button class="button button_chg"><a href="/fashion/pages/Admin/view_deptusers.php">View Department Users</a></button>
 		<button class="button button_chg"><a href="/fashion/pages/Admin/view_ordusers.php">View Ordinary Users</a></button>
 		<button class="button button_chg"><a href="/fashion/pages/Admin/backupf.php">Backup System</a></button>
-		<button class="button button_chg"><a href="#">Restore System</a></button>
+		
 	</div>
 	
 </div>
